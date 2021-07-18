e0988a9f3e680308accb6bd4e4621d8cf45755c5
Muljayan <37667437+Muljayan@users.noreply.github.com>
2021-07-18 23:45:25 +0530

    exceptions for content security policy

--- server.js
+++ server.js
@@ -7,6 +7,26 @@ const app = express();
 // Compression
 app.use(compression());
 app.use(helmet());
+app.use(helmet.contentSecurityPolicy({
+  defaultSrc: [
+    '*',
+    'https://api.altruizt.xyz',
+    'https://altruizt.xyz',
+  ],
+  styleSrc: [
+    '\'self\'',
+    '\'unsafe-inline\'',
+    'https://*.googleapis.com',
+    'https://api.altruizt.xyz',
+    'https://altruizt.xyz',
+  ],
+  scriptSrc: [
+    '\'self\'',
+    '\'unsafe-inline\'',
+    'https://api.altruizt.xyz',
+    'https://altruizt.xyz',
+  ],
+}));
 
 const root = path.join(__dirname, 'build/');
 app.use(express.static(root));
