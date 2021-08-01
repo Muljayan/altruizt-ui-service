91d129fce9be5367dcdca9e44237b21d4fe84138
Muljayan <37667437+Muljayan@users.noreply.github.com>
2021-08-02 00:02:36 +0530

    test push

--- server.js
+++ server.js
@@ -8,7 +8,7 @@ const app = express();
 app.use(compression());
 app.use(helmet());
 app.use(helmet.contentSecurityPolicy({
-  useDefaults: 'true',
+  useDefaults: 'false',
   directives: {
     defaultSrc: [
       '*',
